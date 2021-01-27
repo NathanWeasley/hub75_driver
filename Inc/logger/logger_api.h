12eafc6561d0a27a3d98bd9f4052dc6ac2627a81
NathanWeasley <nathanweasley.work@gmail.com>
2021-01-27 23:47:05 +0800

    initial commit

---
+++ Inc/logger/logger_api.h
@@ -0,0 +1,31 @@
+#ifndef LOGGER_API_H
+#define LOGGER_API_H
+
+#include <stdint.h>
+#include <stdio.h>
+
+#define LOGGER_BUFFER_SIZE      (1024)
+
+typedef enum
+{
+    LOGGER_VERBOSE = 0,
+    LOGGER_INFO,
+    LOGGER_WARNING,
+    LOGGER_ERROR,
+    LOGGER_FATAL,
+
+    LOGGER_LEVEL_CNT,
+    LOGGER_FORCE
+} logger_level_e;
+
+extern char loglevel;
+extern char logbuf[LOGGER_BUFFER_SIZE];
+
+inline void configLoggerLevel(logger_level_e newlevel)
+{
+    loglevel = newlevel;
+}
+
+void report(logger_level_e level, const char * fmt, ...);
+
+#endif
---
+++ Src/logger/logger_api.c
@@ -0,0 +1,28 @@
+#include "logger/logger_api.h"
+#include "stm32f1xx.h"
+#include "stm32f1xx_hal_uart.h"
+#include "usart.h"
+#include <stdarg.h>
+#include <string.h>
+
+char loglevel = LOGGER_INFO;
+char logbuf[LOGGER_BUFFER_SIZE] = "";
+
+void report(logger_level_e level, const char * fmt, ...)
+{
+    va_list valist;
+    int bytes2send = 0;
+
+    if (level < loglevel)
+        return;
+
+    va_start(valist, fmt);
+    bytes2send = snprintf(logbuf, LOGGER_BUFFER_SIZE, fmt, valist);
+    va_end(valist);
+
+    if (bytes2send > 0)
+    {
+        HAL_UART_Transmit(&huart2, logbuf, bytes2send, bytes2send / 20 + 1);
+        HAL_UART_Transmit(&huart2, "\r\n", 2, 1);
+    }
+}
